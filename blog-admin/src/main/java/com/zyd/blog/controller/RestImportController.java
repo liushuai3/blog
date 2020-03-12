ab0986d15cd6d36b7d065631cc8e5d1d03c18259
liushuai3 <liushuai3@asiainfo.com>
2020-03-12 15:48:39 +0800

    增加淘宝联盟导入数据功能

--- blog-admin/src/main/java/com/zyd/blog/controller/RestImportController.java
+++ blog-admin/src/main/java/com/zyd/blog/controller/RestImportController.java
@@ -6,21 +6,20 @@ import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageInfo;
 import com.taobao.api.DefaultTaobaoClient;
 import com.taobao.api.TaobaoClient;
+import com.taobao.api.domain.UatmTbkItem;
 import com.taobao.api.request.TbkUatmFavoritesGetRequest;
+import com.taobao.api.request.TbkUatmFavoritesItemGetRequest;
 import com.taobao.api.response.TbkUatmFavoritesGetResponse;
+import com.taobao.api.response.TbkUatmFavoritesItemGetResponse;
 import com.zyd.blog.business.annotation.BussinessLog;
-import com.zyd.blog.business.entity.Tags;
-import com.zyd.blog.business.enums.ResponseStatus;
-import com.zyd.blog.business.service.BizTagsService;
-import com.zyd.blog.business.vo.TagsConditionVO;
+import com.zyd.blog.business.entity.Article;
+import com.zyd.blog.business.service.BizArticleService;
+import com.zyd.blog.business.vo.ArticleConditionVO;
 import com.zyd.blog.framework.object.PageResult;
 import com.zyd.blog.framework.object.ResponseVO;
-import com.zyd.blog.persistence.beans.BizTags;
 import com.zyd.blog.util.ResultUtil;
-import org.apache.shiro.authz.annotation.Logical;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,7 +41,7 @@ import java.util.Map;
 @RequestMapping("/import")
 public class RestImportController {
     @Autowired
-    private BizTagsService tagsService;
+    private BizArticleService articleService;
 
     //@RequiresPermissions("imports")
     @PostMapping("/list")
@@ -80,7 +79,77 @@ public class RestImportController {
     @PostMapping(value = "/add")
     @BussinessLog("数据导入")
     public ResponseVO add(String favoritesId) {
-
-        return ResultUtil.success("数据导入成功！"+favoritesId);
+        String url = "https://eco.taobao.com/router/rest";
+        String appkey = "28485666";
+        String secret = "feac79d58095283addca7ab933500b89";
+        TaobaoClient client = new DefaultTaobaoClient(url, appkey, secret);
+        TbkUatmFavoritesItemGetRequest req = new TbkUatmFavoritesItemGetRequest();
+        req.setPlatform(1L);
+        req.setPageSize(20L);
+        req.setAdzoneId(110088300188L);
+        req.setUnid(String.valueOf(System.currentTimeMillis()));
+        req.setFavoritesId(Long.valueOf(favoritesId));
+        req.setPageNo(1L);
+        req.setFields("num_iid,title,pict_url,click_url,volume,status,coupon_click_url,reserve_price,zk_final_price");
+        int count = 0;
+        try {
+            TbkUatmFavoritesItemGetResponse rsp = client.execute(req);
+            List<UatmTbkItem> list = rsp.getResults();
+            for (int i = 0; i < list.size(); i++) {
+                UatmTbkItem uatmTbkItem = list.get(i);
+                //商品ID
+                Long num_iid = uatmTbkItem.getNumIid();
+                //商品标题
+                String title = uatmTbkItem.getTitle();
+                //商品主图
+                String pict_url = uatmTbkItem.getPictUrl();
+                //淘客地址
+                String click_url = uatmTbkItem.getClickUrl();
+                //30天销量
+                Long volume = uatmTbkItem.getVolume();
+                //宝贝状态，0失效，1有效；注：失效可能是宝贝已经下线或者是被处罚不能在进行推广
+                Long status = uatmTbkItem.getStatus();
+                //商品优惠券推广链接
+                String coupon_click_url = uatmTbkItem.getCouponClickUrl();
+                //商品一口价格
+                String reserve_price = uatmTbkItem.getReservePrice();
+                //商品折扣价格
+                String zk_final_price = uatmTbkItem.getZkFinalPrice();
+                if(status==1){
+                    ArticleConditionVO vo = new ArticleConditionVO();
+                    Article article = new Article();
+                    Long userId = (Long)SecurityUtils.getSubject().getPrincipal();
+                    article.setUserId(userId);
+                    article.setTitle(title);
+                    vo.setArticle(article);
+                    PageInfo<Article> pageInfo = articleService.findPageBreakByCondition(vo);
+                    if(pageInfo==null){
+                        StringBuilder stringContent = new StringBuilder();
+                        stringContent.append("<p><img src=\"").append(pict_url).append("\" alt=\"\" style=\"box-sizing: inherit; border-style: none; height: auto; max-width: 100%; color: rgb(51, 51, 51); font-family: Helvetica, Arial, &quot;Microsoft Yahei&quot;, &quot;Hiragino Sans GB&quot;, &quot;Heiti SC&quot;, &quot;WenQuanYi Micro Hei&quot;, sans-serif; font-size: 12px; background-color: rgb(238, 238, 238);\"><br mxs=\"pub-threeaU:c\" style=\"box-sizing: inherit; color: rgb(51, 51, 51); font-family: Helvetica, Arial, &quot;Microsoft Yahei&quot;, &quot;Hiragino Sans GB&quot;, &quot;Heiti SC&quot;, &quot;WenQuanYi Micro Hei&quot;, sans-serif; font-size: 12px; background-color: rgb(238, 238, 238);\"></p><pre style=\"box-sizing: inherit; margin-top: 0px; margin-bottom: 0px; white-space: pre-wrap; font-size: 12px; background-color: rgb(238, 238, 238);\">【爆款推荐】").append(title).append(" \n" +
+                                "30天销量达").append(volume).append("件 \n" +
+                                "原价").append(reserve_price).append("元，优惠价仅").append(zk_final_price).append("元\n" +
+                                "-----------------\n" +
+                                "<p style=\"margin-top: 0px; margin-bottom: 16px;\">【立即领券】点击链接即可领券购买:<a href=\"").append(coupon_click_url==null?"":coupon_click_url).append("\" target=\"_blank\" style=\"color: rgb(3, 102, 214);\">").append(coupon_click_url==null?"无":coupon_click_url).append("</a></p><p style=\"margin-top: 0px; margin-bottom: 16px;\">【立即下单】点击链接立即下单：<a href=\"").append(click_url).append("\" target=\"_blank\" style=\"color: rgb(3, 102, 214);\">").append(click_url).append("</a></p></pre>");
+                        count++;
+                        article.setCoverImage(pict_url);
+                        article.setIsMarkdown(false);
+                        article.setContent(stringContent.toString());
+                        article.setTop(false);
+                        article.setTypeId(6L);
+                        article.setStatus(1);
+                        article.setRecommended(false);
+                        article.setOriginal(true);
+                        article.setDescription(title);
+                        article.setKeywords(title);
+                        article.setComment(true);
+                        Long[] tags = {11L};
+                        articleService.publish(article, tags, null);
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return ResultUtil.success("数据导入成功！共导入"+count+"条数据");
     }
 }
