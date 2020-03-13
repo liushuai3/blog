63fcac93757fbe1af298c59c5c01a87c5a1c532b
liushuai3 <liushuai3@asiainfo.com>
2020-03-13 17:43:32 +0800

    增加淘宝联盟导入数据功能

--- blog-admin/src/main/java/com/zyd/blog/controller/RestImportController.java
+++ blog-admin/src/main/java/com/zyd/blog/controller/RestImportController.java
@@ -86,10 +86,11 @@ public class RestImportController {
         String url = taoBaoProperties.url;
         String appkey = taoBaoProperties.appkey;
         String secret = taoBaoProperties.secret;
+        long pageSize = 20L;
         TaobaoClient client = new DefaultTaobaoClient(url, appkey, secret);
         TbkUatmFavoritesItemGetRequest req = new TbkUatmFavoritesItemGetRequest();
         req.setPlatform(1L);
-        req.setPageSize(20L);
+        req.setPageSize(pageSize);
         req.setAdzoneId(110088300188L);
         req.setUnid(String.valueOf(System.currentTimeMillis()));
         req.setFavoritesId(Long.valueOf(favoritesId));
@@ -98,66 +99,76 @@ public class RestImportController {
         int count = 0;
         try {
             TbkUatmFavoritesItemGetResponse rsp = client.execute(req);
-            List<UatmTbkItem> list = rsp.getResults();
-            for (int i = 0; i < list.size(); i++) {
-                UatmTbkItem uatmTbkItem = list.get(i);
-                //商品ID
-                Long num_iid = uatmTbkItem.getNumIid();
-                //商品标题
-                String title = uatmTbkItem.getTitle();
-                //商品主图
-                String pict_url = uatmTbkItem.getPictUrl();
-                //淘客地址
-                String click_url = uatmTbkItem.getClickUrl();
-                //30天销量
-                Long volume = uatmTbkItem.getVolume();
-                //宝贝状态，0失效，1有效；注：失效可能是宝贝已经下线或者是被处罚不能在进行推广
-                Long status = uatmTbkItem.getStatus();
-                //商品优惠券推广链接
-                String coupon_click_url = uatmTbkItem.getCouponClickUrl();
-                //商品一口价格
-                String reserve_price = uatmTbkItem.getReservePrice();
-                //商品折扣价格
-                String zk_final_price = uatmTbkItem.getZkFinalPrice();
-                //优惠卷面额
-                String coupon_info =uatmTbkItem.getCouponInfo();
-                if(status==1){
-                    ArticleConditionVO vo = new ArticleConditionVO();
-                    Article article = new Article();
-                    Long userId = (Long)SecurityUtils.getSubject().getPrincipal();
-                    article.setUserId(userId);
-                    article.setTitle(title);
-                    vo.setArticle(article);
-                    PageInfo<Article> pageInfo = articleService.findPageBreakByCondition(vo);
-                    if(pageInfo==null){
-                        StringBuilder stringContent = new StringBuilder();
-                        stringContent.append("<p><img src=\"").append(pict_url).append("\" alt=\"\" style=\"box-sizing: inherit; border-style: none; height: auto; max-width: 100%; color: rgb(51, 51, 51); font-family: Helvetica, Arial, &quot;Microsoft Yahei&quot;, &quot;Hiragino Sans GB&quot;, &quot;Heiti SC&quot;, &quot;WenQuanYi Micro Hei&quot;, sans-serif; font-size: 12px; background-color: rgb(238, 238, 238);\"><br mxs=\"pub-threeaU:c\" style=\"box-sizing: inherit; color: rgb(51, 51, 51); font-family: Helvetica, Arial, &quot;Microsoft Yahei&quot;, &quot;Hiragino Sans GB&quot;, &quot;Heiti SC&quot;, &quot;WenQuanYi Micro Hei&quot;, sans-serif; font-size: 12px; background-color: rgb(238, 238, 238);\"></p><pre style=\"box-sizing: inherit; margin-top: 0px; margin-bottom: 0px; white-space: pre-wrap; font-size: 12px; background-color: rgb(238, 238, 238);\">【爆款推荐】").append(title).append(" \n" +
-                                "30天销量达").append(volume).append("件 \n" +
-                                "原价").append(reserve_price).append("元，优惠价").append(zk_final_price).append("元");
-                        if(coupon_info!=null && coupon_click_url!=null){
-                            stringContent.append(",还有(").append(coupon_info).append(")的优惠卷可以领取哟！！！\n");
-                        }else {
-                            stringContent.append("\n");
+            //选品库中数据的总条数
+            long totalResults = rsp.getTotalResults();
+            long allPageNo = totalResults%pageSize!=0?(totalResults/pageSize)+1:totalResults/pageSize;
+            for(long j=1 ; j<=allPageNo ; j++){
+                if(j>1){
+                    req.setPageNo(j);
+                    rsp = client.execute(req);
+                }
+                List<UatmTbkItem> list = rsp.getResults();
+                for (int i = 0; i < list.size(); i++) {
+                    UatmTbkItem uatmTbkItem = list.get(i);
+                    //商品ID
+                    Long num_iid = uatmTbkItem.getNumIid();
+                    //商品标题
+                    String title = uatmTbkItem.getTitle();
+                    //商品主图
+                    String pict_url = uatmTbkItem.getPictUrl();
+                    //淘客地址
+                    String click_url = uatmTbkItem.getClickUrl();
+                    //30天销量
+                    Long volume = uatmTbkItem.getVolume();
+                    //宝贝状态，0失效，1有效；注：失效可能是宝贝已经下线或者是被处罚不能在进行推广
+                    Long status = uatmTbkItem.getStatus();
+                    //商品优惠券推广链接
+                    String coupon_click_url = uatmTbkItem.getCouponClickUrl();
+                    //商品一口价格
+                    String reserve_price = uatmTbkItem.getReservePrice();
+                    //商品折扣价格
+                    String zk_final_price = uatmTbkItem.getZkFinalPrice();
+                    //优惠卷面额
+                    String coupon_info =uatmTbkItem.getCouponInfo();
+                    if(status==1){
+                        ArticleConditionVO vo = new ArticleConditionVO();
+                        Article article = new Article();
+                        Long userId = (Long)SecurityUtils.getSubject().getPrincipal();
+                        article.setUserId(userId);
+                        article.setTitle(title);
+                        vo.setArticle(article);
+                        PageInfo<Article> pageInfo = articleService.findPageBreakByCondition(vo);
+                        if(pageInfo==null){
+                            StringBuilder stringContent = new StringBuilder();
+                            stringContent.append("<p><img src=\"").append(pict_url).append("\" alt=\"\" style=\"box-sizing: inherit; border-style: none; height: auto; max-width: 100%; color: rgb(51, 51, 51); font-family: Helvetica, Arial, &quot;Microsoft Yahei&quot;, &quot;Hiragino Sans GB&quot;, &quot;Heiti SC&quot;, &quot;WenQuanYi Micro Hei&quot;, sans-serif; font-size: 12px; background-color: rgb(238, 238, 238);\"><br mxs=\"pub-threeaU:c\" style=\"box-sizing: inherit; color: rgb(51, 51, 51); font-family: Helvetica, Arial, &quot;Microsoft Yahei&quot;, &quot;Hiragino Sans GB&quot;, &quot;Heiti SC&quot;, &quot;WenQuanYi Micro Hei&quot;, sans-serif; font-size: 12px; background-color: rgb(238, 238, 238);\"></p><pre style=\"box-sizing: inherit; margin-top: 0px; margin-bottom: 0px; white-space: pre-wrap; font-size: 12px; background-color: rgb(238, 238, 238);\">【爆款推荐】").append(title).append(" \n" +
+                                    "30天销量达").append(volume).append("件 \n" +
+                                    "原价").append(reserve_price).append("元，优惠价").append(zk_final_price).append("元");
+                            if(coupon_info!=null && coupon_click_url!=null){
+                                stringContent.append(",还有(").append(coupon_info).append(")的优惠卷可以领取哟！！！\n");
+                            }else {
+                                stringContent.append("\n");
+                            }
+                            stringContent.append("-----------------\n" +
+                                    "<p style=\"margin-top: 0px; margin-bottom: 16px;\">【立即领券】点击链接即可领券购买:<a href=\"").append(coupon_click_url==null?"":coupon_click_url).append("\" target=\"_blank\" style=\"color: rgb(3, 102, 214);\">").append(coupon_click_url==null?"无":coupon_click_url).append("</a></p><p style=\"margin-top: 0px; margin-bottom: 16px;\">【立即下单】点击链接立即下单：<a href=\"").append(click_url).append("\" target=\"_blank\" style=\"color: rgb(3, 102, 214);\">").append(click_url).append("</a></p></pre>");
+                            count++;
+                            article.setCoverImage(pict_url);
+                            article.setIsMarkdown(false);
+                            article.setContent(stringContent.toString());
+                            article.setTop(false);
+                            article.setTypeId(6L);
+                            article.setStatus(1);
+                            article.setRecommended(false);
+                            article.setOriginal(true);
+                            article.setDescription(title);
+                            article.setKeywords(title);
+                            article.setComment(true);
+                            Long[] tags = {11L};
+                            articleService.publish(article, tags, null);
                         }
-                        stringContent.append("-----------------\n" +
-                                "<p style=\"margin-top: 0px; margin-bottom: 16px;\">【立即领券】点击链接即可领券购买:<a href=\"").append(coupon_click_url==null?"":coupon_click_url).append("\" target=\"_blank\" style=\"color: rgb(3, 102, 214);\">").append(coupon_click_url==null?"无":coupon_click_url).append("</a></p><p style=\"margin-top: 0px; margin-bottom: 16px;\">【立即下单】点击链接立即下单：<a href=\"").append(click_url).append("\" target=\"_blank\" style=\"color: rgb(3, 102, 214);\">").append(click_url).append("</a></p></pre>");
-                        count++;
-                        article.setCoverImage(pict_url);
-                        article.setIsMarkdown(false);
-                        article.setContent(stringContent.toString());
-                        article.setTop(false);
-                        article.setTypeId(6L);
-                        article.setStatus(1);
-                        article.setRecommended(false);
-                        article.setOriginal(true);
-                        article.setDescription(title);
-                        article.setKeywords(title);
-                        article.setComment(true);
-                        Long[] tags = {11L};
-                        articleService.publish(article, tags, null);
                     }
                 }
             }
+
         }catch (Exception e){
             e.printStackTrace();
         }
